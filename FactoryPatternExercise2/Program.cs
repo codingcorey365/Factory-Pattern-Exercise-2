ba832986388a9934c18100a706dd34a39ccc667e
Corey Schroeder <codingcorey365@gmail.com>
2023-12-04 10:48:08 -0600

    test

--- FactoryPatternExercise2/Program.cs
+++ FactoryPatternExercise2/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("test!");
+            Console.WriteLine("test!");
         }
     }
 }
