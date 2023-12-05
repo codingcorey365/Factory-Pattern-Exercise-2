2839cc39b4b4dfdbbf24eef19b000cda652f2046
Corey Schroeder <codingcorey365@gmail.com>
2023-12-05 08:28:31 -0600

    added the switch case statement the the userinput logic of which database they would like to choose.

--- FactoryPatternExercise2/Program.cs
+++ FactoryPatternExercise2/Program.cs
@@ -7,33 +7,61 @@ namespace FactoryPatternExercise2
         static void Main(string[] args)
         {
             bool correctInput = true;
-            do
-            {
-            Console.WriteLine("Hello, what type of database would you like to use? Enter: list, sql or mongo for the database type you would like to utilize.");
+            do  // if satement
+            {   // if satement
+                Console.WriteLine("Hello, what type of database would you like to use? Enter: list, sql or mongo for the database type you would like to utilize.");
 
-            string databaseType = Console.ReadLine();
+                string databaseType = Console.ReadLine();
 
-                // ---------------------------------------  If Else Statement ---------------------------------------------
+                // ---------------------------------------  If Else Statement ---------------------------------------------------
                 // Console.WriteLine("-------------------------------------------------------------------------------------------");
-                //if (databaseType == "list".ToLower() || databaseType == "sql".ToLower() || databaseType == "mongo".ToLower())
+                if (databaseType == "list".ToLower() || databaseType == "sql".ToLower() || databaseType == "mongo".ToLower())
+                {
+                    IDataAccess database = DataAccessFactory.GetDataAccessType(databaseType);
+
+                    List<Product> products = database.LoadData();
+                    database.SaveData();
+
+                    foreach (var item in products)
+                    {
+                        Console.WriteLine($"Name: {item.Name} Price: {item.Price}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Not a valid input");
+                    correctInput = false;
+                }
+                // Console.WriteLine("-------------------------------------------------------------------------------------------");
+
+                // ---------------------------------------  Switch Case Statement -----------------------------------------------
+                // Console.WriteLine("-------------------------------------------------------------------------------------------");
+                //switch (databaseType.ToLower())
                 //{
-                //    IDataAccess database = DataAccessFactory.GetDataAccessType(databaseType);
-
-                //    List<Product> products = database.LoadData();
-                //    database.SaveData();
-
-                //    foreach (var item in products)
-                //    {
-                //        Console.WriteLine($"Name: {item.Name} Price: {item.Price}");
-                //    }
+                //    case "list":
+                //        break;
+                //    case "sql":
+                //        break;
+                //    case "mongo":
+                //        break;
+                //    default:
+                //        Console.WriteLine("Invalid input. Please enter: list, sql, or mongo for the database type you would like to utilize.");
+                //        correctInput = false;
+                //        break;
                 //}
-                //else
+
+                //IDataAccess database = DataAccessFactory.GetDataAccessType(databaseType);
+
+                //List<Product> products = database.LoadData();
+                //database.SaveData();
+
+                //foreach (var item in products)
                 //{
-                //    Console.WriteLine("Not a valid input");
-                //    correctInput = false;
+                //    Console.WriteLine($"Name: {item.Name} Price: {item.Price}");
                 //}
                 // Console.WriteLine("-------------------------------------------------------------------------------------------");
-            } while (correctInput == false);
+
+            } while (correctInput == false); // if satement
         }
     }
 }
