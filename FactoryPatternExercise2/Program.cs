242b169d55aa6c8c49ac13473708f7e157be422c
Corey Schroeder <codingcorey365@gmail.com>
2023-12-04 21:32:16 -0600

    Initial push now that code base is mostly implemented in to the factory feature branch

--- FactoryPatternExercise2/Program.cs
+++ FactoryPatternExercise2/Program.cs
@@ -1,13 +1,36 @@
-﻿namespace FactoryPatternExercise2
+﻿using System.Threading.Channels;
+
+namespace FactoryPatternExercise2
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("test!");
-            Console.WriteLine("test!");
-            Console.WriteLine("test!");
-            Console.WriteLine("test!");
+            Console.WriteLine("Hello, what type of database would you like to use? Enter: list, sql or mongo for the database type you would like to utilize.");
+
+            string databaseType = Console.ReadLine();
+            bool correctInput = true;
+            //do
+            //{
+
+            //} while (correctInput = false);
+            if (databaseType == "list".ToLower() || databaseType == "sql".ToLower() || databaseType == "mongo".ToLower())
+            {
+                IDataAccess database = DataAccessFactory.GetDataAccessType(databaseType);
+
+                List<Product> products = database.LoadData();
+                database.SaveData();
+
+                foreach (var item in products)
+                {
+                    Console.WriteLine($"Name: {item.Name} Price: {item.Price}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Not a valid input");
+                correctInput = false;
+            }
         }
     }
 }
